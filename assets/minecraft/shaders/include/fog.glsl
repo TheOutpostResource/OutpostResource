da70ffe0f658c2ad25c5a16032f54317cafa2b45
Jellychicken <mgwon805@naver.com>
2022-05-15 14:15:43 +0900

    fixed shader

--- assets/minecraft/shaders/include/fog.glsl
+++ assets/minecraft/shaders/include/fog.glsl
@@ -1,12 +1,19 @@
 #version 150
 
 vec4 linear_fog(vec4 inColor, float vertexDistance, float fogStart, float fogEnd, vec4 fogColor) {
-    
-    if (vertexDistance <= fogStart)
+    if (vertexDistance <= fogStart) {
         return inColor;
-
+    }
     float fogValue = vertexDistance < fogEnd ? smoothstep(fogStart, fogEnd, vertexDistance) : 1.0;
     return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
+}
+
+vec4 linear_fog_translucent(vec4 inColor, float vertexDistance, float fogStart, float fogEnd, vec4 fogColor) {
+    if (vertexDistance <= fogStart) {
+        return inColor;
+    }
+    float fogValue = vertexDistance < fogEnd ? smoothstep(fogStart, fogEnd, vertexDistance) : 1.0;
+    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a*(1.0-fogValue));
 }
 
 float linear_fog_fade(float vertexDistance, float fogStart, float fogEnd) {
@@ -15,18 +22,9 @@ float linear_fog_fade(float vertexDistance, float fogStart, float fogEnd) {
     } else if (vertexDistance >= fogEnd) {
         return 0.0;
     }
-
-    return smoothstep(fogEnd, fogStart, vertexDistance);
+    return smoothstep(fogStart, fogEnd, vertexDistance);
 }
 
-//1.18.1
-float cylindrical_distance(mat4 modelViewMat, vec3 pos) {
-    float distXZ = length((modelViewMat * vec4(pos.x, 0.0, pos.z, 1.0)).xyz);
-    float distY = length((modelViewMat * vec4(0.0, pos.y, 0.0, 1.0)).xyz);
-    return max(distXZ, distY);
-}
-
-//1.18.2+
 float fog_distance(mat4 modelViewMat, vec3 pos, int shape) {
     if (shape == 0) {
         return length((modelViewMat * vec4(pos, 1.0)).xyz);
